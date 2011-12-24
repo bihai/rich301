13c1b3104914294f37bdc36f1b8b14c794edb025
GuoLin <fallen_lord@126.com>
2011-12-24 23:56:11 +0800

    Added secure

---
+++ app/controllers/Games.java
@@ -0,0 +1,25 @@
+package controllers;
+
+import play.mvc.Controller;
+
+/**
+ * Control the games.
+ * 
+ * @author GuoLin
+ *
+ */
+public class Games extends Controller {
+
+    public static void start(String name) {
+        
+    }
+
+    public static void join() {
+        
+    }
+
+    public static void destroy() {
+        
+    }
+
+}
