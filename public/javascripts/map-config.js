0d0a07c92774170078df04f1e024b03709eb7860
seasontop <xiechaoning@gmail.com>
2011-12-25 00:59:33 +0800

    add the public path on the main.html file

--- public/javascripts/map-config.js
+++ public/javascripts/map-config.js
@@ -1,4 +1,4 @@
-var background = R301.constants.STATIC_PATH + '/images/bg.jpg',
+var background = R301.constants.PUBLIC_PATH + '/images/bg.jpg',
 	map = [
 	{ id: 1, type: 'estate', price: 100, name: '', road: '上海', owner: '', level: 1, next: '', prev: '', size: 1, parent: '', photoPositionX: '', photoPositionY: '' },
 	{ id: 1, type: 'shop', name: '', road: '上海', next: '', prev: '', size: 1, parent: '', photoPositionX: '', photoPositionY: '' }
