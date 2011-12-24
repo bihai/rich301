b8682f11a43e88464e68ce7b2884279577019bfa
GuoLin <fallen_lord@126.com>
2011-12-24 23:20:08 +0800

    Simplely added base javascript

---
+++ public/javascripts/rich301.js
@@ -0,0 +1,28 @@
+/**
+ * Rich 301 namespace.
+ * @type object
+ */
+var R301 = R301 || {};
+
+/**
+ * Utilities namespace.
+ * @type object
+ * @namespace R301
+ */
+R301.util = R301.util || {};
+
+/**
+ * Thirdparty namespace.
+ * <p>
+ * All thirdparty libraries should under this namespace.</p>
+ * @type object
+ * @namespace R301
+ */
+R301.thirdparty = R301.thirdparty || {};
+
+/**
+ * Module namespace to store all business module class.
+ * @type object
+ * @namespace R301
+ */
+R301.module = R301.module || {};
