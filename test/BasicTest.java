bf2db71f828740312fe5f0b0f2944453394d96a5
seanxiaoxiao <seanxiaoxiao@hotmail.com>
2011-12-25 01:14:58 +0800

    Delete the verbose file

--- test/BasicTest.java
+++ test/BasicTest.java
@@ -1,6 +1,7 @@
 import org.junit.*;
 import java.util.*;
 import play.test.*;
+import util.RichUtil;
 import models.*;
 
 public class BasicTest extends UnitTest {
@@ -10,4 +11,9 @@ public class BasicTest extends UnitTest {
         assertEquals(2, 1 + 1);
     }
 
+    @Test
+    public void testMapJson() {
+    	GameMap map = MapGenerator.generateTestMap();
+    	System.out.println(RichUtil.mapToJson(map));
+    }
 }
