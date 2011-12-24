12e5334dc4e1fc3abff14c7c3ba8d7fee64c1e4f
GuoLin <fallen_lord@126.com>
2011-12-25 01:00:22 +0800

    Added create/join/list game logic

--- app/controllers/Games.java
+++ app/controllers/Games.java
@@ -1,6 +1,13 @@
 package controllers;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import models.Game;
+import models.Player;
 import play.mvc.Controller;
+import controllers.Secure.Security;
 
 /**
  * Control the games.
@@ -10,11 +17,34 @@ import play.mvc.Controller;
  */
 public class Games extends Controller {
 
-    public static void start(String name) {
-        
+    private static Map<String, Game> games = new HashMap<String, Game>();
+
+    public static void list() {
+        Collection<Game> games = Games.games.values();
+        render(games);
     }
 
-    public static void join() {
+    public static void create(String name) {
+        if (games.containsKey(name)) {
+            return;
+        }
+        Game game = new Game(name);
+        String connected = Security.connected();
+        Player player = new Player(connected);
+        game.addPlayer(player);
+        games.put(name, game);
+        list();
+    }
+
+    public static void join(String gameName) {
+        String connected = Security.connected();
+        Game game = games.get(gameName);
+        Player player = new Player(connected);
+        game.addPlayer(player);
+        list();
+    }
+    
+    public static void start(String name) {
         
     }
 
---
+++ app/models/Player.java
@@ -0,0 +1,17 @@
+package models;
+
+/**
+ * Player model.
+ * 
+ * @author GuoLin
+ *
+ */
+public class Player {
+
+    public String name;
+
+    public Player(String name) {
+        this.name = name;
+    }
+    
+}
