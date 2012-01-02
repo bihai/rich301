322c8d5dbf4a8ca8d4baff396dfd4d294ef4ffce
GuoLin <fallen_lord@126.com>
2012-01-03 02:34:38 +0800

    Now player states can be shown on the statusbar

--- public/javascripts/statusbar.js
+++ public/javascripts/statusbar.js
@@ -11,11 +11,16 @@
      * @constructor
      */
     R301.module.Statusbar = function(options) {
-        var me = this, $window;
+        var me = this, $window, container;
         this.options = $.extend({}, this.options, options);
+        container = this.options.container;
+        this.options.playersContainer = container.find("#players");
+        this.options.detailsContainer = container.find("#details");
+
+        // Bind global events
         $window = $(window);
-        $window.bind("gameStart", function(event, players) {
-            me.onGameStart(players);
+        $window.bind("startGame", function(event, game) {
+            me.onGameStart(game);
         });
     };
 
@@ -27,15 +32,63 @@
              * Container of statusbar.
              * @type jQuery
              */
-            container: null
+            container: null,
+
+            /**
+             * Players details container.
+             * @type jQuery
+             */
+            playersContainer: null,
+
+            /**
+             * Game details container.
+             * @type jQuery
+             */
+            detailsContainer: null
 
         },
+
+        /**
+         * Game that the statusbar belongs to.
+         * @type object
+         */
+        game: {},
 
         /**
          * Render status bar.
          */
         render: function() {
-            
+            this._renderDetails();
+            this._renderPlayers();
+        },
+
+        /**
+         * Render details.
+         */
+        _renderDetails: function() {
+            var game = this.game, container = this.options.detailsContainer;
+            container.find("h1").text(game.name);
+            container.find("em").text(game.round);
+        },
+
+        /**
+         * Render players.
+         */
+        _renderPlayers: function() {
+            var game = this.game, container = this.options.playersContainer,
+                players = game.players;
+            $.each(players, function(i, player) {
+                var htmls = [], role = player.role,
+                    face = R301.constants.ROLE_FACE_ROOT + role.face;
+                htmls.push('<li ' + (player.active ? 'class="active"' : '') + '>');
+                htmls.push('<img src="' + face + '" />');
+                htmls.push('<div class="details">');
+                htmls.push('<h3>' + player.name + '</h3>');
+                htmls.push('<em>$' + player.cash + '</em>');
+                htmls.push('</div>')
+                htmls.push('</li>');
+                $(htmls.join("")).appendTo(container);
+            });
         },
 
         /**
@@ -43,7 +96,16 @@
          * @param {array} game Game information
          */
         onGameStart: function(game) {
-            
+            this.game.id = game.id;
+            this.game.name = game.name;
+            this.game.round = game.round;
+            this.game.players = game.players;
+            $.each(game.players, function(i, player) {
+                if (player.id == game.currentPlayer.id) {
+                    player.active = true;
+                }
+            });
+            this.render();
         }
 
     };
